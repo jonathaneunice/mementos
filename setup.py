9a59ccae965e45e71d1fa4b429615e8c24a1cf87
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-24 11:42:16 -0400

    1.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def lines(text):
 
 setup(
     name='mementos',
-    version='1.1.2',
+    version='1.2.0',
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description='Memoizing metaclass. Drop-dead simple way to create cached objects',
